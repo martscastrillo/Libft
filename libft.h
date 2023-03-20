248fe77c79238d122defc1880e79b2c7699e7383
martscastrillo <mart.castrillo@gmail.com>
2023-03-20 15:01:52 +0100

    add strdup and calloc

--- libft.h
+++ libft.h
@@ -15,9 +15,11 @@
 
 # include <string.h>
 # include <stdio.h>
+# include <stdlib.h> 
 
 int	ft_atoi(char *str);
 void *ft_bzero(void *s, size_t n);
+void *ft_calloc(size_t count, size_t size);
 int ft_isalnum(int c);
 int	ft_isalpha(int c);
 int	ft_isascii(int c);
@@ -29,6 +31,7 @@ void *ft_memcpy(void * dst, const void * src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 char *ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s1);
 size_t	ft_strlcat(char *  dst, const char *  src, size_t dstsize);
 size_t ft_strlcpy(char *  dst, const char *  src, size_t dstsize);
 size_t ft_strlen(const char *s);
