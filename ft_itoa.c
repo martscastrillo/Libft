25f7cda2fc2404d245120594d0b1e2a13cc20749
martscastrillo <mart.castrillo@gmail.com>
2023-03-29 19:52:00 +0200

    add itoa whith norminette passed

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: martcast <martcast@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 19:34:36 by martcast          #+#    #+#             */
-/*   Updated: 2023/03/27 19:34:38 by martcast         ###   ########.fr       */
+/*   Updated: 2023/03/29 19:50:05 by martcast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /* DESCRIPCIÓN/
@@ -16,58 +16,47 @@
 #include "libft.h"
 #include <stdio.h>
 
-char *ft_itoa(int n)
+int	ft_extra( int n)
 {
-    int temp;
-    int i;
-    char *str;
-    int len;
-  
-    len = 0;
-    temp = n;
-    i = 0;
-    while (temp != 0)
-    {
-        len++;
-        temp = temp / 10;
-    }
+	int	len;
+	int	temp;
 
-    if (n < 0 || n == 0)
-        len++;
-    str = malloc((len + 1) * sizeof(char));
-    if (str == NULL)
-        return (NULL);
-    str[len] = '\0';
-    if (n < 0)
-    {
-        str[0] = '-';
-        n = -n;
-    }
-   i = 0;
-
-   /* strdup hacerlo antes del malloc*/
-     if (n == -2147483648)
-    {
-        char *minInt = "-2147483648";
-        while (minInt[i] != '\0')
-        {
-            str[i] = minInt[i];
-            i++;
-        }
-   /*      hacer un return y quitar el else */
-    } 
-else{
-    i = len - 1;
-    while (i >= 0  )
-    {
-        if (str[i] != '-')
-        {
-            str[i] = (n % 10) + '0';
-            n /= 10;
-        }
-        i--;
-    }
+	len = 0;
+	temp = n;
+	while (temp != 0)
+	{
+		len++;
+		temp = temp / 10;
+	}
+	if (n < 0 || n == 0)
+		len++;
+	return (len);
 }
-    str[i] = '\0';
-    return str;
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
+	len = ft_extra(n);
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	str[len] = '\0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		n = -n;
+	}
+	while (len-- >= 0)
+	{
+		if (str[len] != '-')
+		{
+			str[len] = (n % 10) + '0';
+			n /= 10;
+		}
+	}
+	return (str);
 }
