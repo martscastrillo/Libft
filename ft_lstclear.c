dfeff3ff670e6ccc990487877e9ea6a8c0a5e06b
martscastrillo <mart.castrillo@gmail.com>
2023-04-15 14:48:58 +0200

    update readme

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,12 +1,7 @@
 **************************
 /* DESCRIPCIÓN/
-Elimina y libera el nodo ’lst’ dado y todos los
-consecutivos de ese nodo, utilizando la función
-’del’ y free(3).
-Al final, el puntero a la lista debe ser NULL.
- * lst: la dirección de un puntero a un nodo.
-del: un puntero a función utilizado para eliminar
-el contenido de un nodo.
+* Elimina y libera el nodo ’lst’ dado y todos los consecutivos de ese nodo, utilizando la función ’del’ y free(3).
+Al final, el puntero a la lista debe ser NULL. lst: la dirección de un puntero a un nodo. del: un puntero a función utilizado para eliminar el contenido de un nodo.
  * VALOR DEVUELTO
  * NADA.*/
 void ft_lstclear(t_list **lst, void (*del)(void *))
--- ft_lstiter.c
+++ ft_lstiter.c
@@ -1,8 +1,6 @@
 ******************************
 /* DESCRIPCIÓN/
- * Itera la lista ’lst’ y aplica la función ’f’ en el
-contenido de cada nodo. lst: un puntero al primer nodo.
-f: un puntero a la función que utilizará cada nodo.
+ * Itera la lista ’lst’ y aplica la función ’f’ en el contenido de cada nodo. lst: un puntero al primer nodo. f: un puntero a la función que utilizará cada nodo.
  * VALOR DEVUELTO
  * NADA.*/
 void ft_lstiter(t_list *lst, void (*f)(void *))
